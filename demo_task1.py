de7b8ee78b7f64489a497342ca2ee0be52aa12e5
karthik40404 <karpython123@gmail.com>
2024-07-25 10:05:04 +0530

    'db'

--- demo_task1.py
+++ demo_task1.py
@@ -22,15 +22,44 @@ while True:
         con.commit()
     elif choice==2:
         emp_id=int(input('enter ID :'))
+        data=con.execute('select*from staff ')
+        print("{:<10}{:<10}{:<10}{:<10}{:<10}".format("emp_id","name","age","salary","phno"))
+        print('_'*45)
+        for i in data:
+            print("{:<10}{:<10}{:<10}{:<10}{:<10}".format(i[0],i[1],i[2],i[3],i[4]))
+        con.commit()
+
+    
+    elif choice==3:
+        emp_id=int(input('enter ID :'))
+        data=con.execute('select*from staff where emp_id',(emp_id,))
+        print("{:<10}{:<10}{:<10}{:<10}{:<10}".format("emp_id","name","age","salary","phno"))
+        print('_'*45)
+        for i in data:
+            print("{:<10}{:<10}{:<10}{:<10}{:<10}".format(i[0],i[1],i[2],i[3],i[4]))
+        con.commit()
+
+    elif choice==4:
+        emp_id=int(input('enter ID :'))
         name=str(input('enter name: '))
         age=int(input('enter age:'))
         salary=float(input('enter salary:'))
         phno=int(input('enter phno:'))
-    
-    
+        con.execute('update into staff (name,age,salary,phno) values(?,?,?,?,?)',(name,age,salary,phno))
+        con.commit()
 
+    elif choice==5:
+        emp_id=int(input('enter ID :'))
+        data=con.execute('delete*from staff where emp_id',(emp_id,))
+        con.commit()
 
-        data=con.execute('select*from staff ')
+    elif choice==6:
+        print("you are exited")
+        break
+    else:
+        print('invalid')  
+
+    data=con.execute('select*from staff ')
     print("{:<10}{:<10}{:<10}{:<10}{:<10}".format("emp_id","name","age","salary","phno"))
     print('_'*45)
     for i in data:
